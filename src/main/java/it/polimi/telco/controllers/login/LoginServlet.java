93d987535fde7b3608a4a76d7a7509f797553e63
CampeanTudor <campeantudor97@gmail.com>
2022-01-07 19:19:36 +0100

    created endpoints for login

---
+++ src/main/java/it/polimi/telco/controllers/login/LoginServlet.java
@@ -0,0 +1,32 @@
+package it.polimi.telco.controllers.login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(
+        name = "loginOperation",
+        description = "JSP Servlet With Annotations",
+        urlPatterns = {"/loginOperation"}
+)
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/successfulLoginRequest.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        request.setAttribute("text", username + password);
+        doGet(request, response);
+    }
+}
---
+++ src/main/webapp/loginForm.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: campe
+  Date: 1/7/2022
+  Time: 6:11 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <form action="loginOperation" method="post">
+        <input type="text" name="username" />
+        <input type="text" name="password" />
+        <input type="submit" />
+    </form>
+</head>
+<body>
+
+</body>
+</html>
